9e9860e116afe9fe0c6213d109415f6f73e63a8f
Roy Mwangi <royimwangi@gmail.com>
2025-08-29 20:56:05 +0300

    Project setup

---
+++ app/app/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>Login to my app</Text>
+        </View>
+    )
+}
+
+export default SignIn
---
+++ app/hooks/useStorageState.tsx
@@ -0,0 +1,4 @@
+import { useState, useEffect, useCallback } from "react";
+import * as SecureStore from 'expo-secure-store';
+import { Platform } from "react-native";
+
